f54fcd8174705a0fd5c09d04a5ebd476ea077d07
zihui <101386735+N0nEscapeSh3d0w@users.noreply.github.com>
2023-09-17 15:02:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def update_Student():
 
     statement = "UPDATE Student SET email = %s, programme = %s, group = %d, cgpa = %lf, intern_batch = %s, currentAddress = %s, contactNo = %s, personalEmail = %s, homeAddress = %s, homePhone = %s  WHERE stud_id = %s;"
     cursor = db_conn.cursor()
-    cursor.execute(statement, email, programme, group, cgpa, intern_batch, currentAddress, contactNo, personalEmail, homeAddress, homePhone, stud_id))
+    cursor.execute(statement (email, programme, group, cgpa, intern_batch, currentAddress, contactNo, personalEmail, homeAddress, homePhone, stud_id))
     db_conn.commit()  # Commit the changes to the database
 
     return render_template("/student.html/" + stud_id)
