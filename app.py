df95b6d87ae27dd6a586da2f7a9208148c01a45c
zihui <101386735+N0nEscapeSh3d0w@users.noreply.github.com>
2023-09-17 16:42:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def update_Student():
     cursor.execute(statement, (programme, student_group, cgpa, password, intern_batch, currentAddress, contactNo, personalEmail, homeAddress, homePhone, stud_id))
     db_conn.commit()  # Commit the changes to the database
 
-    return render_template("/student.html/" + stud_id)
+    return redirect("/student.html/" + stud_id)
 
 @app.route("/internshipPublication", methods=['GET', 'POST'])
 def publichInternPage():
